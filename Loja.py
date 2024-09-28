336bc47efb6d913d961a213d72beb670ab7f468a
Victor Lima <adriano_hb@hotmail.com>
2024-09-28 14:56:55 -0300

    esqueci

--- Loja.py
+++ Loja.py
@@ -15,47 +15,71 @@
     #      8 – 18 vezes: 12% de juros ao mês. 
 
 ValorCompra = float(input("qual o valor da compra em R$?"))
-
-
 print("-"*20)
 print("1- A vista")
 print("2- A prazo")
 print("-"*20)
-
-
 FormaPagamento = input("qual forma de pagamento?")
-
+print("-"*20)
 if FormaPagamento == "1":
+  print("-"*20)
   print("forma escolinda foi a vista")
-
-
-  
-elif FormaPagamento == "2":
-  print("forma escolhida foi a prazo")
-else:
-  print("forma invalida")
-
 print("-"*20)
-print("1- sim")
-print("2- não")
-print("-"*20)
-correçao = input("sua escolha esta correta?")
+if ValorCompra <= 500:
+  print("-"*20) 
+  print(f"seus valor de compra foi de {ValorCompra - (ValorCompra * 0.1)}")
+  print("-"*20)
+elif ValorCompra <= 999:
+  print(f"seus valor de compra foi de {ValorCompra - (ValorCompra * 0.15)}")
+  print("-"*20)
+elif ValorCompra >1000:
+  print(f"seus valor de compra foi de {ValorCompra - (ValorCompra * 0.2)}")
+  print("-"*20)
 
-if correçao == "1":
-  print("ok vamos proseguir")
 
-elif correçao == "2":
-  print("ok")
 
-print("-"*20)
-print("1- sim")
-print("2- não")
-print("-"*20)
 
-correçao = input("sua escolha esta correta?")
 
-if correçao == "1":
-  print("ok vamos proseguir")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# elif FormaPagamento == "2":
+#   print("forma escolhida foi a prazo")
+# else:
+#   print("forma invalida")
+
+# print("-"*20)
+# print("1- sim")
+# print("2- não")
+# print("-"*20)
+# correçao = input("sua escolha esta correta?")
+
+# if correçao == "1":
+#   print("ok vamos proseguir")
+
+# elif correçao == "2":
+#   print("ok")
+
+# print("-"*20)
+# print("1- sim")
+# print("2- não")
+# print("-"*20)
+
+# correçao = input("sua escolha esta correta?")
+
+# if correçao == "1":
+#   print("ok vamos proseguir")
 
 
 
